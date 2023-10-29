c355677920d68dd112fc8b11572f70cf3dbf6754
Reinhard Javera Maheswara <71541330+reinhardjavera@users.noreply.github.com>
2023-10-29 14:25:43 +0700

    Add files via upload

---
+++ WebProgramming/Week5/03_composer/index.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+use HelloWorld\Greetings;
+
+echo Greetings::sayHelloWorld();
---
+++ WebProgramming/Week5/03_composer/src/Greetings.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace HelloWorld;
+
+class Greetings
+{
+
+    public static function sayHelloWorld()
+    {
+        return 'Hello World';
+    }
+}
