78625b5a6673bc96bd967d65bd1670491603b3f6
Reinhard Javera Maheswara <71541330+reinhardjavera@users.noreply.github.com>
2023-10-31 18:06:50 +0700

    Update web.php

--- WebProgramming/Week8/my-app/routes/web.php
+++ WebProgramming/Week8/my-app/routes/web.php
@@ -50,25 +50,6 @@ Route::get('/students/prodi/{prodi?}', function ($theProdi = "IF") {
 
 //Latihan Tugas
 
-/*
-Route::get('/students/{nim}/{nama}/{prodi}/{Fakultas}', function ($theNim, $theNama, $theProdi, $theFakultas) {
-    return "NIM: " . $theNim . "<br /> Nama: " . $theNama . "<br /> Prodi: " . $theProdi . "<br /> Fakutlas: " . $theFakultas;
-}); */
-
 Route::get('fakultas/{fakultas}/prodi/{prodi}/mhs/{nim}/{nama}', function ($theFakultas, $theProdi, $theNim, $theNama) {
     return "NIM: " . $theNim . "<br /> Nama: " . $theNama . "<br /> Prodi: " . $theProdi . "<br /> Fakultas: " . $theFakultas;
 });
-
- /*
-Route::get('mhs/{nama}', function ($theNama) {
-    return "<br /> Nama: " . $theNama;
-});
-
-Route::get('prodi/{prodi}', function ($theProdi) {
-    return "<br /> Prodi: " . $theProdi;
-});
-
-Route::get('fakultas/{fakultas}', function ($theFakultas) {
-    return "<br /> Fakultas: " . $theFakultas;
-});
-*/
